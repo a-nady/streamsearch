71a7b11c04f085d515b9ad03994b879e390dca7a
a-nady <17anady@gmail.com>
2021-10-29 01:55:00 -0400

    Milestone 1

--- db.js
+++ db.js
@@ -1,25 +1,35 @@
 const mongoose = require('mongoose');
 
-const User = new mongoose.Schema({
-    username: String,
-    password: String,
-    watchlists: [Watchlist]
+
+
+const Movie = new mongoose.Schema({
+  	// title of the movie
+    title: String,
+  	// since some content is on multiple services, schematype should be an array here
+    service: [String],
+    dateReleased: Number,
+    description: String,
+  	// if user watched it or not
+    watched: Boolean
 });
 
 const Watchlist = new mongoose.Schema({
+  	// name of the watch list as well as an array of all the content being stored by the user in that watchlist
     name: String,
-    stocks: [Stock]
+    movies: [Movie]
 });
 
-const Stock = new mongoose.Schema({
-    ticker: String,
-    name: String,
-    priceAdded: Number,
-    priceNow: Number,
-    change: Number,
-    description: String
+const User = new mongoose.Schema({
+  	// store user information
+    username: String,
+    password: String,
+  	// watchlist will contain a list of seperate lists of movies/tv
+    watchlists: [Watchlist]
 });
+
+
+
 
 mongoose.model('User', User);
 mongoose.model('Watchlist', Watchlist);
-mongoose.model('Stock', Stock);
+mongoose.model('Movie', Movie);
