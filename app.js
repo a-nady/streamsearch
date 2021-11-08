72dfb23b7d843115ef1735f5b44315cca9f8cac9
a-nady <17anady@gmail.com>
2021-11-08 00:38:07 -0500

    initial app creation and deployment

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const path = require("path")
 const app = express();
 //const Movie = mongoose.model("User");
 app.set('view engine', 'hbs');
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render('index');
 });
 
-app.listen(3000);
-
+const port = process.env.PORT || 3000;
+app.listen( port, () => {
+    console.log( `Server started on port ${ port }.` );
+} );
