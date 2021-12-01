22146c6e6d624781ee673aca2ddf4695893097e7
a-nady <17anady@gmail.com>
2021-12-01 12:54:59 -0500

    Fix collapsable navbar so it works on navbar, make it more mobile friendly with viewport :D

--- views/layout.jsx
+++ views/layout.jsx
@@ -10,16 +10,22 @@ class Layout extends React.Component {
             <html lang="en">
             <head>
                 <title>StreamSearch</title>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity=""
+                        crossOrigin="anonymous"></script>
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
                       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
                       crossOrigin="anonymous"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui"/>
             </head>
             <body style={{
-                background:'linear-gradient(to right, #430089, #82ffa1)'
+                background:'linear-gradient(to right, #430089, #82ffa1)',
+                height: '100%',
+                overflow: 'auto'
             }}>
             {this.props.children}
             </body>
             </html>
+
         )
     }
 }
--- views/navbar.jsx
+++ views/navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
-function NavBar(state = false) {
+
+function NavBar(loggedIn = false) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
             <div className="container-fluid">
                 <a href="/" className="navbar-brand">StreamSearch</a>
-                <button type="button" className="navbar-toggler" data-bs-toggle="collapse"
-                        data-bs-target="#navbarCollapse">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+
+                <div className="dropdown">
+                    <button className="navbar-toggler" type="button" id="dropdownMenuButton1"
+                             aria-expanded="false" data-bs-toggle="collapse"
+                            data-bs-target="#navbarCollapse">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
                 <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                     <div className="navbar-nav">
                         <a href="/search" className="nav-item nav-link active">Search</a>
-                        {state ? (
+                        {loggedIn ? (
                             <a href="/logout" className="nav-item nav-link active">Logout</a>
                         ) : (
                             <>
@@ -24,7 +31,11 @@ function NavBar(state = false) {
 
                 </div>
             </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                    crossOrigin="anonymous"></script>
         </nav>
+
     )
 }
 
